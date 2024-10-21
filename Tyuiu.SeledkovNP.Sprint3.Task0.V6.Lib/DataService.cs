6175e66416e30789909befc73b948af5045917b1
Tyuiu.SeledkovNP <Tyuiu.SeledkovNP@outlook.com>
2024-10-21 09:36:47 +0500

    Add project files.

---
+++ Tyuiu.SeledkovNP.Sprint3.Task0.V6.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.SeledkovNP.Sprint3.Task0.V6.Lib
+{
+    public class DataService : ISprint3Task0V6
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+
+            double SumSeries = 1;
+            int k;
+            for (k = startValue; k <= stopValue; k++)
+            {
+                SumSeries = SumSeries * (1 / Math.Pow((Math.Cos(5) + 1), 2));
+            }
+            return Math.Round(SumSeries, 3);
+        }
+
+    }
+    
+}
---
+++ Tyuiu.SeledkovNP.Sprint3.Task0.V6/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.SeledkovNP.Sprint3.Task0.V6.Lib;
+namespace Tyuiu.SeledkovNP.Sprint3.Task0.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+            Console.WriteLine( "Спринт #3 | Выполнила: Селедков Н. П. | Смартю-24-1");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнил: Селедков Никита Павлович | Смартб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет произведение   *");
+            Console.WriteLine("* ряда по формуле                                                         *");
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 1;
+            int stopValue = 15;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + ds.GetMultiplySeries(startValue, stopValue));
+            Console.ReadKey();
+
+        }
+    }
+}
